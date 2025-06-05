d0037bf56e2af937df74cf0a5eb93e313d35f98e
ayman jarmoune <ayman.jarmoune@gmail.com>
2025-06-05 14:06:16 +0100

    fixing env error

--- run_automation.sh
+++ run_automation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Script to trigger the automation Python script
 
-# Navigate to the project directory (optional, but good practice if the script relies on relative paths)
-# cd /root/n8n-docker/automation_project
+cd /root/n8n-docker/automation_project
 
-# Execute the Python script
-# Replace 'python3' with the specific path to your Python interpreter if needed (e.g., /root/n8n-docker/automation_project/pyEnv/bin/python)
-python src/utils/automation_trigger_ubuntu.py
+# Optional Git pull
+git pull origin main
+
+# Run automation using specific Python
+/root/n8n-docker/automation_project/pyEnv310/bin/python3 src/utils/automation_trigger_ubuntu.py
 
 echo "Automation script triggered."
