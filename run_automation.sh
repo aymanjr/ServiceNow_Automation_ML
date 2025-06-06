e086de25dff44bb960876cba05ebfc32ebae7607
ayman jarmoune <ayman.jarmoune@gmail.com>
2025-06-06 18:36:15 +0100

    changes

--- run_automation.sh
+++ run_automation.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-cd /root/n8n-docker/automation_project
+echo "🚀 Starting automation..."
 
-# Optional Git pull
-git pull origin main
+project_root="/app/data"
 
-# Run automation using specific Python
-/root/n8n-docker/automation_project/pyEnv310/Scripts/python.exe src/utils/automation_trigger_ubuntu.py
+latest_file=$(ls -t "$project_root/data/raw/"unlabeled_tickets__*.csv | head -n 1)
+start_date=$(echo "$latest_file" | grep -oP 'Start_\K[0-9_]+')
+end_date=$(echo "$latest_file" | grep -oP 'End_\K[0-9_]+')
+start_date=${start_date//_/-}
+end_date=${end_date//_/-}
 
-echo "Automation script triggered."
+cmds="
+cd \"$project_root\" &&
+python3 src/classifier.py --predict \"data/raw/$(basename "$latest_file")\" --output \"data/processed/predictions.csv\" &&
+python3 src/utils/data_to_json.py --input data/processed/predictions.csv --start-date $start_date --end-date $end_date &&
+python3 src/utils/charts.py --input data/processed/service_summary.json --csv data/processed/predictions.csv --output data/charts &&
+python3 src/utils/visualization.py --input data/processed/service_summary.json --output data/reports/
+"
+
+echo -e "📦 Commands to run:\n$cmds"
+
+eval "$cmds"
+echo "[✔] Done."
